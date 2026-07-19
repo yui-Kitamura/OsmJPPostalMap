109b9bdf13091897dfd942a8e79d7a5e5eaf5db5
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-20 00:50:55 +0900

    fix 400 bad request getからpostに変えた影響

--- app/src/main/java/pro/eng/yui/android/osmjppostalmap/data/api/OverpassApi.java
+++ app/src/main/java/pro/eng/yui/android/osmjppostalmap/data/api/OverpassApi.java
@@ -1,10 +1,12 @@
 package pro.eng.yui.android.osmjppostalmap.data.api;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 public interface OverpassApi {
+    @FormUrlEncoded
     @POST("interpreter")
-    Call<OverpassResponse> query(@Query("data") String data);
+    Call<OverpassResponse> query(@Field("data") String data);
 }
