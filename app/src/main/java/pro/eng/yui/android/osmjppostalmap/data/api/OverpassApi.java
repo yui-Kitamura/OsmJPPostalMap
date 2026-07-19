f67dbcc2570b0396075ffa18d1e86eb5480bda06
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-19 23:52:57 +0900

    fix HTTP406 content-type

--- app/src/main/java/pro/eng/yui/android/osmjppostalmap/data/api/OverpassApi.java
+++ app/src/main/java/pro/eng/yui/android/osmjppostalmap/data/api/OverpassApi.java
@@ -1,12 +1,12 @@
 package pro.eng.yui.android.osmjppostalmap.data.api;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface OverpassApi {
-    @Headers("Accept: application/json")
-    @GET("interpreter")
+    @Headers({"Content-Type: text-plain", "Accept: application/json"})
+    @POST("interpreter")
     Call<OverpassResponse> query(@Query("data") String data);
 }
