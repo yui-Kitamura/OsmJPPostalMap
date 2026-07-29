7e395f676f72fbec0c992378ffd0d8677d54e06d
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-30 05:27:59 +0900

    import整理

--- app/src/main/java/pro/eng/yui/android/osmjppostalmap/ui/MainActivity.java
+++ app/src/main/java/pro/eng/yui/android/osmjppostalmap/ui/MainActivity.java
@@ -13,7 +13,6 @@ import org.osmdroid.views.MapView;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
