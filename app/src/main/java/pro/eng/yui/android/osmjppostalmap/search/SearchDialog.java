49059d151f27621461349257c5d25c8fba5997dc
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-08-01 23:21:35 +0900

    import不足ケア

--- app/src/main/java/pro/eng/yui/android/osmjppostalmap/search/SearchDialog.java
+++ app/src/main/java/pro/eng/yui/android/osmjppostalmap/search/SearchDialog.java
@@ -9,10 +9,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
